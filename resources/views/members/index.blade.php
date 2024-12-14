11a4a6af4e91c085ebaf4b2cde37f4c6aabf8922
meli1230 <melisa_marian@yahoo.fr>
2024-12-14 14:53:44 +0200

    members index and create

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -1,1 +1,33 @@
-<?php
+<table>
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Profession</th>
+        <th>Company</th>
+        <th>Status</th>
+        <th>Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    @foreach ($members as $member)
+        <tr>
+            <td>{{ $member->name }}</td>
+            <td>{{ $member->email }}</td>
+            <td>{{ $member->profession }}</td>
+            <td>{{ $member->company }}</td>
+            <td>{{ $member->status }}</td>
+            <td><a href="{{ route('members.edit', $member->id) }}">Edit</a>
+                <form action="{{ route('members.destroy', $member->id) }}" method="POST"
+                      style="display: inline-block;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+
+    @endforeach     </tbody>
+</table>
+{{ $members->links() }}
