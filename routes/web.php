b9b8d972387e0bf2330a1abf8e582f7544ffd56e
meli1230 <melisa_marian@yahoo.fr>
2024-12-14 16:40:34 +0200

    routes set

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\SuccessStoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/members/index', [MemberController::class, '/members/index']);
+Route::get('/members/create', [MemberController::class, '/members/create']);
+
+Route::get('/events/index', [EventController::class, '/events/index']);
+Route::get('/events/create', [EventController::class, '/events/create']);
+
+Route::get('/successStories/index', [SuccessStoryController::class, '/successStories/index']);
+Route::get('/successStories/create', [SuccessStoryController::class, '/successStories/create']);
