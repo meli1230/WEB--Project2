5d82f3d9917bdc8109c643898b8953e5f11d2679
meli1230 <melisa_marian@yahoo.fr>
2024-12-14 20:46:08 +0200

    styling, header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,5 @@
-<?php
+<footer class="pt-5 my-5 text-muted">
+    Created by
+    <a href="https://codeanddeploy.com">Code And Deploy</a>
+    &middot; &copy; {{ date('Y') }}
+</footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,21 @@
-<?php
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route('members.index') }}">Brand Name</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle
+             navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('members.index') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('members.create') }}">Create</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
