d31ccf70e08ba05b7d936158acfba42bf9466ea1
meli1230 <melisa_marian@yahoo.fr>
2024-12-14 20:22:39 +0200

    routes modified

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,43 @@ use App\Http\Controllers\MemberController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\SuccessStoryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Route::get('/members/index', [MemberController::class, 'index']);
+///*Route::get('/members/create', [MemberController::class, 'create']);*/
+//
+//Route::get('/events/index', [EventController::class, 'index']);
+///*Route::get('/events/create', [EventController::class, 'create']);*/
+//
+//Route::get('/successStories/index', [SuccessStoryController::class, 'index']);
+///*Route::get('/successStories/create', [SuccessStoryController::class, 'create']);*/
+//
+//Route::prefix('members')->group(function () {
+//    /* Route::get('/', [MemberController::class, 'index'])->name('members.index'); // Listare membri*/
+//    Route::get('/create', [MemberController::class, 'create'])->name('members.create'); // Formular creare membru
+//    Route::post('/', [MemberController::class, 'store'])->name('members.store'); // Salvare membru nou
+//    Route::get('/{id}/edit', [MemberController::class, 'edit'])->name('members.edit'); // Formular editare
+//    Route::patch('/{id}', [MemberController::class, 'update'])->name('members.update'); // Actualizare membru
+//    Route::delete('/{id}', [MemberController::class, 'destroy'])->name('members.destroy'); // Ștergere membru
+//});
+// Default route
+Route::get('/', [MemberController::class, 'index'])->name('home');
 
-Route::get('/members/index', [MemberController::class, 'index']);
-/*Route::get('/members/create', [MemberController::class, 'create']);*/
-
-Route::get('/events/index', [EventController::class, 'index']);
-/*Route::get('/events/create', [EventController::class, 'create']);*/
-
-Route::get('/successStories/index', [SuccessStoryController::class, 'index']);
-/*Route::get('/successStories/create', [SuccessStoryController::class, 'create']);*/
-
+// Members routes
 Route::prefix('members')->group(function () {
-    /* Route::get('/', [MemberController::class, 'index'])->name('members.index'); // Listare membri*/
-    Route::get('/create', [MemberController::class, 'create'])->name('members.create'); // Formular creare membru
-    Route::post('/', [MemberController::class, 'store'])->name('members.store'); // Salvare membru nou
-    Route::get('/{id}/edit', [MemberController::class, 'edit'])->name('members.edit'); // Formular editare
-    Route::patch('/{id}', [MemberController::class, 'update'])->name('members.update'); // Actualizare membru
-    Route::delete('/{id}', [MemberController::class, 'destroy'])->name('members.destroy'); // Ștergere membru
+    Route::get('/', [MemberController::class, 'index'])->name('members.index');
+    Route::get('/create', [MemberController::class, 'create'])->name('members.create');
+    Route::post('/', [MemberController::class, 'store'])->name('members.store');
+    Route::get('/{id}/edit', [MemberController::class, 'edit'])->name('members.edit');
+    Route::patch('/{id}', [MemberController::class, 'update'])->name('members.update');
+    Route::delete('/{id}', [MemberController::class, 'destroy'])->name('members.destroy');
 });
 
-
+// Events routes
 Route::prefix('events')->group(function () {
-    /*Route::get('/', [EventController::class, 'index'])->name('events.index');*/
+    Route::get('/', [EventController::class, 'index'])->name('events.index');
     Route::get('/create', [EventController::class, 'create'])->name('events.create');
     Route::post('/', [EventController::class, 'store'])->name('events.store');
     Route::get('/{id}/edit', [EventController::class, 'edit'])->name('events.edit');
@@ -37,8 +49,9 @@ Route::prefix('events')->group(function () {
     Route::delete('/{id}', [EventController::class, 'destroy'])->name('events.destroy');
 });
 
+// Success Stories routes
 Route::prefix('successStories')->group(function () {
-    /*Route::get('/', [SuccessStoryController::class, 'index'])->name('successStories.index');*/
+    Route::get('/', [SuccessStoryController::class, 'index'])->name('successStories.index');
     Route::get('/create', [SuccessStoryController::class, 'create'])->name('successStories.create');
     Route::post('/', [SuccessStoryController::class, 'store'])->name('successStories.store');
     Route::get('/{id}/edit', [SuccessStoryController::class, 'edit'])->name('successStories.edit');
