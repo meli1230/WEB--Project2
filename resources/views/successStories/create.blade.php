c469abf6ac95270c7a08e29abcb7833326c8e544
meli1230 <melisa_marian@yahoo.fr>
2024-12-14 16:28:37 +0200

    create for all

--- resources/views/successStories/create.blade.php
+++ resources/views/successStories/create.blade.php
@@ -1,1 +1,17 @@
-<?php
+<form action="{{ route('successStories.store') }}" method="POST">
+
+    @csrf
+    <label for="title">Title:</label>
+    <input type="text" name="title" required>
+    <br>
+
+    <label for="story">Story:</label>
+    <textarea name="story" required></textarea>
+    <br>
+
+    <label for="member_id">Member Name:</label>
+    <input type="text" name="member_id" required>
+
+    <br>
+    <button type="submit">Add Member</button>
+</form>
