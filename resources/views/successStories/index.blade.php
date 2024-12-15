eb398659749469196d4e130c8255d4342991c739
meli1230 <melisa_marian@yahoo.fr>
2024-12-15 11:19:36 +0200

    success story by name

--- resources/views/successStories/index.blade.php
+++ resources/views/successStories/index.blade.php
@@ -1,37 +1,37 @@
 @extends('layouts.master')
 @section('content')
 
-
-<table>
-    <thead>
-    <tr>
-        <th>Title</th>
-        <th>Story</th>
-        <th>Member Name</th>
-        <th>Actions</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach ($successStories as $successStory)
+    <table>
+        <thead>
         <tr>
-            <td>{{ $successStory->title }}</td>
-            <td>{{ $successStory->story }}</td>
-            <td>{{ $successStory->member_id }}</td>
-            <td>
-                <a href="{{ route('successStories.edit', $successStory->id) }}">Edit</a>
-                <form action="{{ route('successStories.destroy', $successStory->id) }}"
-                      method="POST"
-                      style="display: inline-block;"
-                      onsubmit="return confirm('Are you sure you want to delete this member?');">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
-            </td>
+            <th>Title</th>
+            <th>Story</th>
+            <th>Member ID</th>
+            <th>Member Name</th>
+            <th>Actions</th>
         </tr>
-    @endforeach
-    </tbody>
-</table>
-{{ $successStories->links() }}
+        </thead>
+        <tbody>
+        @foreach ($successStories as $successStory)
+            <tr>
+                <td>{{ $successStory->title }}</td>
+                <td>{{ $successStory->story }}</td>
+                <td>{{ $successStory->member->name ?? 'Unknown' }}</td> <!-- Display Member Name -->
+                <td>
+                    <a href="{{ route('successStories.edit', $successStory->id) }}">Edit</a>
+                    <form action="{{ route('successStories.destroy', $successStory->id) }}"
+                          method="POST"
+                          style="display: inline-block;"
+                          onsubmit="return confirm('Are you sure you want to delete this story?');">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{ $successStories->links() }}
 
 @endsection
