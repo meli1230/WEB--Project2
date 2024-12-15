75177c5eed4208bc6db14e2573309c2101e16d24
meli1230 <melisa_marian@yahoo.fr>
2024-12-15 13:26:44 +0200

    styling works

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content=" …">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Women Techpower</title>
     @include('partials.styles')
 </head>
